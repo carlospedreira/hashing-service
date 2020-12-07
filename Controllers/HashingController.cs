11696d8509dda743fddad3ec93bc9c4b738d903a
Carlos Pedreira <carlospedreira@outlook.com>
2020-12-07 01:11:46 +0000

    Add a SHA256 hashing service.

---
+++ Controllers/HashingController.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HashingService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HashingController : ControllerBase
+    {
+        [HttpGet]
+        public string Hash([FromBody] string value)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(value);
+
+            using (var algorithm = SHA256.Create())
+            {
+                var hashedBytes = algorithm.ComputeHash(bytes);
+
+		return GetHashString(hashedBytes);
+            }
+        }
+
+        private static string GetHashString(byte[] bytes)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var b in bytes)
+                sb.Append(b.ToString("X2"));
+
+            return sb.ToString();
+        }
+    }
+
+}
